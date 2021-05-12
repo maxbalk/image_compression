c2ad48bc2ce720499d90b5172b2764896919a743
max <maximumbalk@gmail.com>
2021-05-12 01:02:49 -0500

    vertical first, row sums

--- le_gall.py
+++ le_gall.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 import math
 img = cv2.imread('lena.jpg',0)
 img = img.astype(np.double)
-
+from copy import deepcopy, copy
 
 # define filter coefficients
 lo_forward = np.array([-1/8, 2/8, 6/8, 2/8, -1/8])
@@ -35,48 +35,46 @@ def decompose(img):
 
 def analysis(img):
     bands = decompose(img)
-    bands = np.rot90(bands, -1)
-    bands = decompose(bands)
     bands = np.rot90(bands, 1)
+    bands = decompose(bands)
+    bands = np.rot90(bands, -1)
     return bands
 
-decomp_level = 1
-y_out = img
-for i in range(decomp_level):
-    x_dim = y_out.shape[0]
-    y_dim = y_out.shape[1]
-    y_out = analysis(y_out)
-    #y_out = y_out[:int(x_dim/2),:int(y_dim/2)]
+decomp_level = 2
+y_out = deepcopy(img)
+x_dim = y_out.shape[0]
+y_dim = y_out.shape[1]
+for i in range(decomp_level):    
+    y_out[:int(x_dim/(2**i)),:int(y_dim/(2**i))] = analysis(y_out[:int(x_dim/(2**i)),:int(y_dim/(2**i))])
 
-plt.imshow(y_out)
-plt.show()
+# plt.imshow(y_out)
+# plt.show()
 
 def reconstruct(img):
     rows = []
-    for row in img:
+    for i, row in enumerate(img):
+        g0 = row[:int(len(row)/2)]
+        g1 = row[int(len(row)/2):]
         # upsample. do first and second halves serparately 
-        g0 = np.insert(row, range(1,len(row)+1), 0)
-        g1 = np.insert(row, range(1,len(row)+1), 0)
+        g0 = np.insert(g0, range(1,len(g0)+1), 0)
+        g1 = np.insert(g1, range(1,len(g1)+1), 0)
         # apply filter
         g0 = transform(g0, hi_inverse)
         g1 = transform(g1, lo_inverse)
         # add row values together, not append
-        rows.append(np.append(g0,g1))
-    return np.array(rows)
+        img[i] = np.sum([g0,g1], axis=0)
+    return img
 
 def synthesis(img):
-    bands = reconstruct(img)
+    bands = np.rot90(img, 1)
+    bands = reconstruct(bands)
     bands = np.rot90(bands, -1)
     bands = reconstruct(bands)
-    bands = np.rot90(bands, 1)
     return bands
 
-x_hat = y_out
-for i in range(decomp_level):
-    x_hat = synthesis(x_hat)
-    x_dim = x_hat.shape[0]
-    y_dim = x_hat.shape[1]
-    #x_hat = x_hat[:int(x_dim/2),:int(y_dim/2)]
+x_hat = deepcopy(y_out)
+for i in reversed(range(decomp_level)):
+    x_hat[:int(x_dim/(2**i)),:int(y_dim/(2**i))] = synthesis(x_hat[:int(x_dim/(2**i)),:int(y_dim/(2**i))])
 
 plt.imshow(x_hat)
 plt.show()
